1bf54164c48b16e6c559c1ee2020f9d59eefa9fd
Tin Vukšan <tin.vuksan@vuv.hr>
2022-09-13 17:45:46 +0200

    finished tutorial project

--- PracticeAppWeb/Controllers/CategoryController.cs
+++ PracticeAppWeb/Controllers/CategoryController.cs
@@ -37,11 +37,82 @@ namespace PracticeAppWeb.Controllers
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
+                TempData["success"] = "Category created successfully";
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             } 
             return View(obj);
             
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if(categoryFromDb == null) 
+            { 
+                return NotFound(); 
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The display order cannot match the category name!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                TempData["success"] = "Category updated successfully";
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+
+            var obj = _db.Categories.Find(id); 
+            if(obj==null)
+            {
+                return NotFound();
+            }
+                _db.Categories.Remove(obj);
+            TempData["success"] = "Category deleted successfully";
+            _db.SaveChanges();
+                return RedirectToAction("Index");
+            
+            
+
         }
     }
 }
