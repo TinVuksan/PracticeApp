afec0ebb53330520b8b7936d8aba48876e1c675a
Tin Vukšan <tin.vuksan@vuv.hr>
2022-09-13 13:43:08 +0200

    design, create category, server side validation

--- PracticeAppWeb/Controllers/CategoryController.cs
+++ PracticeAppWeb/Controllers/CategoryController.cs
@@ -17,5 +17,31 @@ namespace PracticeAppWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The display order cannot match the category name!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            } 
+            return View(obj);
+            
+        }
     }
 }
